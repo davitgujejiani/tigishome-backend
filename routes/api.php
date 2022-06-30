0975ef04784152e5cf43426be944102de20b7765
Davit Gujejiani <66127957+DavitGujejiani@users.noreply.github.com>
2022-06-30 18:23:24 +0400

    added test api route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('hello/{name}', function($name) {
+    return response()->json("Hi $name");
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
